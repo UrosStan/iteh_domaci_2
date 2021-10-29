11028f85a96b02b5dcd406f658d80d7110277ddf
Uros Stanimirovic <uros.stanimirovic@blinking.id>
2021-10-29 15:39:30 +0200

    Minnor fix

--- handler/update.php
+++ handler/update.php
@@ -5,7 +5,7 @@ require "../model/prijava.php";
 if (
     (isset($_POST['predmet']) && isset($_POST['katedra']) && isset($_POST['sala']) && isset($_POST['datum']) && isset($_POST['id'])) {
         $obj = new Prijava(null, $_POST['predmet'], $_POST['katedra'], $_POST['sala'], $_POST['datum']);
-        ($obj->update($_POST['id'], $conn))? echo "Uspesno izmenjena prijava": echo "Neuspesno izmenjena prijava"
+        ($obj->update($_POST['id'], $conn))? echo "Success": echo "Failed"
 }
 
 ?> 
